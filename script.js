e8460e423d86bcf3993d06cd7448d5f3d05ad4a5
showrov6567 <hello30id@gmail.com>
2026-07-21 17:55:25 +0600

    Prevent multiple claims of daily reward

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 let balance = 0;
+let claimed = false;
 
 const rewardBtn = document.getElementById("rewardBtn");
 const balanceText = document.querySelector(".card p");
 
 rewardBtn.addEventListener("click", function () {
-    balance += 10;
-    balanceText.innerHTML = balance + " Coins";
-    alert("🎉 You received 10 Coins!");
+
+    if (claimed === false) {
+        balance += 10;
+        balanceText.innerHTML = balance + " Coins";
+        alert("🎉 You received 10 Coins!");
+        claimed = true;
+        rewardBtn.innerHTML = "✅ Reward Claimed";
+    } else {
+        alert("⏳ You already claimed today's reward!");
+    }
+
 });
