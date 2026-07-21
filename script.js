65df5f639c7e7c546c296c9259c7d463efddccf0
showrov6567 <hello30id@gmail.com>
2026-07-21 17:53:06 +0600

    Update reward button functionality to add coins

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+let balance = 0;
+
 const rewardBtn = document.getElementById("rewardBtn");
+const balanceText = document.querySelector(".card p");
 
 rewardBtn.addEventListener("click", function () {
-    alert("🎉 Daily Reward Claimed!");
+    balance += 10;
+    balanceText.innerHTML = balance + " Coins";
+    alert("🎉 You received 10 Coins!");
 });
